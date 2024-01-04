098699da323df91abc69fbc660d1405e33098fce
Solomon Mphasha <solomonramsy@icloud.com>
2024-01-03 18:48:28 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+ 
+/**
+ * swap_int - take two variable integers and swap them
+ * swap and store address of a
+ * swap and store address of b
+ * retur: 0
+ */
+ void swap_int(int *a, int *b)
+{
+int swap
+change = *b
+*b = a*
+change *a
+} 
