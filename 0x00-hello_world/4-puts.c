2ee547bb7fa6758137f460a9bed6dc0842a9ff35
Solomonramsy <solomonramsy@icloud.com>
2024-01-27 04:26:04 -0800

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * Entry point - A program that print a line with puts function
- *
+n
  * Return: 0
  */
 int main(void)
 {
 puts("\"Programming is like buiding a multilingual puzzle.");
 return (0);
-}
+i}
