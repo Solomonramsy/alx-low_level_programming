82c66e178f7b01f7de7133202d1d026d7e5ecc9c
Solomonramsy <solomonramsy@icloud.com>
2024-01-28 05:00:46 -0800

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-printf("with proper grammar, but the outcomes is a piece of art,\n");
+#include <stdio.h>
+/**
+ * main - prints a string of stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
