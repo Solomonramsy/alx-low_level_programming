0d62a45d71e42458d897749faca8b28fbf9993a7
Solomonramsy <solomonramsy@icloud.com>
2024-02-17 08:32:37 -0800

    hello world

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - access point 
+ * main - function to print a putchar
  *
- * restore = 0
- * /
- 
-int main(void) 
+ * Return: 0 (success)
+ */
+
+int main(void)
 {
-_putchar(/n);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
