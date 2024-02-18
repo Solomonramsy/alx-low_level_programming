453c1fd83aed13ef930aea515176d661fef24564
Solomonramsy <solomonramsy@icloud.com>
2024-02-18 04:34:55 -0800

    hello world

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabets x10
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+char ch;
+int i;
+i = 0;
+while (i < 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+i++;
+}
+}
